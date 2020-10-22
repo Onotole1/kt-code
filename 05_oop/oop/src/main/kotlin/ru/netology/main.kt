2207089ca0172a1de6a6ef2130b8ac87bc4baecc
Onotole1 <anatolij.sp@yandex.ru>
2020-10-23 01:27:19 +0700

    sealed class

--- 05_oop/oop/src/main/kotlin/ru/netology/main.kt
+++ 05_oop/oop/src/main/kotlin/ru/netology/main.kt
@@ -1,6 +1,9 @@
 package ru.netology
 
+import ru.netology.data.NewPost
 import ru.netology.data.Post
+import ru.netology.data.RegularPost
+import ru.netology.data.Repost
 import ru.netology.service.WallService
 
 fun main() {
@@ -18,16 +21,31 @@ fun main() {
 //    val (id, _, author) = original
 //    println("$id, $author")
 
-    val original = Post(
+    val original: Post = RegularPost(
         id = 1,
         authorId = 1,
         authorName = "Нетология",
         content = "Новая Нетология",
         created = 1590075360,
-        likes = 22
+        likes = 22,
+        null
+        )
+
+    val repost: Post = Repost(
+        id = 1,
+        authorId = 1,
+        authorName = "Нетология",
+        content = "Новая Нетология",
+        created = 1590075360,
+        likes = 22,
+        original
     )
-    val service = WallService()
-    service.add(original)
+
+    val authorId = when (repost) {
+        is RegularPost -> repost.authorId
+        is Repost -> repost.authorId
+        NewPost -> repost.id
+    }
 }
 
 
